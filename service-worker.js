09f8c2328dd6794be23fe778c58befcaf1404258
Justdabit14 <afeefasree14@gmail.com>
2025-08-21 16:52:05 +0800

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,11 +10,27 @@ self.addEventListener("install", event => {
     })
   );
 });
-
+const CACHE_NAME = "inventory-cache-v2";  // bump version number
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
+  );
+
+});
+self.addEventListener("install", event => {
+  self.skipWaiting(); // activate new SW right away
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
